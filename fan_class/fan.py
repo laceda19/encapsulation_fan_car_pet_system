245cae055e55de62aa4989dd62c3a4898caddd96
laceda19 <gelailaceda@gmail.com>
2026-06-13 11:52:30 -0700

    fix   te spacin a - '- -

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -3,36 +3,30 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
-
     def __init__(self):
         self.__speed = Fan.SLOW
         self.__on = False
         self.__radius = 5
         self.__color = "blue"
-
     def get_speed(self):
         return self.__speed
-
     def get_on(self):
         return self.__on
-
     def get_radius(self):
         return self.__radius
-
     def get_color(self):
         return self.__color
-
-        # Setters
-
+    # Setters
     def set_speed(self, speed):
         self.__speed = speed
-
     def set_on(self, on):
         self.__on = on
-
     def set_radius(self, radius):
         self.__radius = radius
-
     def set_color(self, color):
         self.__color = color
-        
+def display_info(self):
+    print("Speed:", self.__speed)
+    print("Radius:", self.__radius)
+    print("Color:", self.__color)
+    print("On:", self.__on)
--- fan_class/test_fan.py
+++ fan_class/test_fan.py
@@ -1,12 +1,10 @@
 from fan import Fan
-
 # First fan
 fan1 = Fan()
 fan1.set_speed(Fan.FAST)
 fan1.set_radius(10)
 fan1.set_color("yellow")
 fan1.set_on(True)
-
 # Second fan
 fan2 = Fan()
 fan2.set_speed(Fan.MEDIUM)
@@ -18,11 +16,14 @@ print("Speed:", fan1.get_speed())
 print("Radius:", fan1.get_radius())
 print("Color:", fan1.get_color())
 print("On:", fan1.get_on())
-
 print()
-
 print("===== FAN 2 =====")
 print("Speed:", fan2.get_speed())
 print("Radius:", fan2.get_radius())
 print("Color:", fan2.get_color())
 print("On:", fan2.get_on())
+print("===== FAN 1 =====")
+fan1.display_info()
+print()
+print("===== FAN 2 =====")
+fan2.display_info()
