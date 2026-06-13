805f8bbd1e152ae5533786d978245af0c5737d1c
laceda19 <gelailaceda@gmail.com>
2026-06-13 11:18:06 -0700

    Create test program for pet information

---
+++ pet_information_system/test_pet.py
@@ -0,0 +1,16 @@
+from pet import Pet
+
+my_pet = Pet()
+
+name = input("Enter pet name: ")
+animal_type = input("Enter animal type: ")
+age = int(input("Enter pet age: "))
+
+my_pet.set_name(name)
+my_pet.set_animal_type(animal_type)
+my_pet.set_age(age)
+
+print("\nPet Information")
+print("Name:", my_pet.get_name())
+print("Type:", my_pet.get_animal_type())
+print("Age:", my_pet.get_age())
