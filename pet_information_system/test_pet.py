847083687e2d56bd62b2b629b149be3d91f35be2
laceda19 <gelailaceda@gmail.com>
2026-06-13 11:43:47 -0700

    fix the code

--- pet_information_system/test_pet.py
+++ pet_information_system/test_pet.py
@@ -1,15 +1,12 @@
 from pet import Pet
-
 my_pet = Pet()
 my_pet.display_information()
 name = input("Enter pet name: ")
 animal_type = input("Enter animal type: ")
 age = int(input("Enter pet age: "))
-
 my_pet.set_name(name)
 my_pet.set_animal_type(animal_type)
 my_pet.set_age(age)
-
 print("\nPet Information")
 print("Name:", my_pet.get_name())
 print("Type:", my_pet.get_animal_type())
